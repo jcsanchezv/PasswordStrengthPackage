272b9ee25e72ea9ebf9b785617b28b6d0b795b34
jcsanchezv <jcsanchezv@msn.com>
2016-08-02 14:56:46 +0200

    Traducción español

---
+++ src/Schuppo/lang/es/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+            "letters" => "La :attribute debe incluir, al menos, una letra.",
+            "case_diff" => "La :attribute debe contener mayúsculas y minúsculas.",
+            "numbers" => "La :attribute debe incluir, al menos, un número.",
+            "symbols" => "La :attribute debe incluir, al menos, un símbolo."
+];
